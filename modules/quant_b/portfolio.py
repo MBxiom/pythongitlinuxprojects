21768d6729c8516e692647ca255643e5c015a304
MBxiom <martinbouleuc@gmail.com>
2025-12-04 08:56:28 +0100

    portfolio.py

--- modules/quant_b/portfolio.py
+++ modules/quant_b/portfolio.py
@@ -2,25 +2,18 @@ import pandas as pd
 import numpy as np
 
 def simulate_portfolio(data, weights):
-    """
-    Simule l'évolution d'un portefeuille.
-    Arguments:
-        data (pd.DataFrame): Tableau des prix (celui que vous venez de télécharger)
-        weights (dict): Répartition (ex: {'AAPL': 0.5, 'TSLA': 0.5})
-    Retourne:
-        pd.Series: La valeur du portefeuille au fil du temps (base 100)
-    """
-    # 1. Normaliser les prix (faire commencer toutes les actions à 1.0)
-    # Cela permet de comparer Apple (200$) et Google (150$) sur la même base
+    
+    # 1) normalisation des prix
+    # pour comparer apple (200 euros et google 150 euros sur la même base) 
     normalized_data = data / data.iloc[0]
 
-    # 2. Préparer la liste des poids dans le même ordre que les colonnes
+    # 2) Preparation de la liste des poids avec le même ordre que les colonnes
     weight_list = []
     for ticker in data.columns:
         weight_list.append(weights.get(ticker, 0)) # 0 si pas de poids défini
 
-    # 3. Calculer la valeur pondérée (Produit matriciel)
-    # Formule : (Prix_A * Poids_A) + (Prix_B * Poids_B) ...
+    # 3)calcul de la valeur pondérée avec un Produit matriciel
+    
     portfolio_value = normalized_data.dot(weight_list)
 
     # On multiplie par 100 pour dire "Si j'avais investi 100€ au début"
