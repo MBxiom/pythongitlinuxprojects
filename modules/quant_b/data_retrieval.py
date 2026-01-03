b73609252435ac2a62b58378a00ecd8a3fe298ae
Seunghyeon-Kim-0920 <rlatmdgus0920@gmail.com>
2026-01-03 02:06:17 +0100

    Update print statements for data loading

--- modules/quant_b/data_retrieval.py
+++ modules/quant_b/data_retrieval.py
@@ -2,13 +2,10 @@ import yfinance as yf
 import pandas as pd
 
 def get_multiple_data(tickers):
-    print(f" chargement des données pour : {tickers}...")
+    print(f" loading data for : {tickers}...")
     
-    # chargement des données sur 5jours toutes les 5 minutes 
     data = yf.download(tickers, period="5d", interval="5m", progress=False)
     
-    #que la colonne 'Close' : Prix de fermeture
-    # Si un seul ticker est demandé, yfinance change le format on pallie ça :
     if len(tickers) == 1:
         return data['Close'].to_frame() 
         
